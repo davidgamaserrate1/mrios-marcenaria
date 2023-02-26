2d6412b76719e57c295404fb15c10e1d108221a0
davidgamaserrate1 <david.gama@ufms.br>
2023-02-26 09:53:04 -0300

    feat: header

---
+++ src/compents/header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './header-style.css'
+
+import logo from '../../assets/logo.png'
+
+const Header = () =>{
+    return (
+        <header className="header">
+            <img className="header-logo" src={logo}/>
+            <ul className="header-list">
+                 
+                <li className="header-item">Quem somos</li>
+                <li className="header-item">Vantagens</li>
+                <li className="header-item">Projetos</li>
+                <li className="header-item">Contato</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header

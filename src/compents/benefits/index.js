a77ba86c73148206b184b01655269ec6682b3790
david.serrate <david.serrate@redetendencia.com.br>
2023-03-20 12:03:26 -0400

    feat: align items cards

--- src/compents/benefits/index.js
+++ src/compents/benefits/index.js
@@ -5,15 +5,12 @@ import BenefitItem from './benefit-item'
 const Benefits = () => {
     return (
         <section id="benefits">
-            <BenefitItem titulo='Economia' description='preços acessiveis e as melhores condições de pagamento' />
-            <BenefitItem titulo='Agilidade' description='Tempo de entrega' />
-            <BenefitItem />
-            <BenefitItem />
-            <BenefitItem />
-            <BenefitItem />
-            <BenefitItem />
-            <BenefitItem />
-            <BenefitItem />
+            <div className='benefit-items'>
+
+                <BenefitItem titulo='Economia' description='preços acessiveis e as melhores condições de pagamento' />
+                <BenefitItem titulo='Agilidade' description='Tempo de entrega' />
+            </div>
+
         </section>
     )
 }
